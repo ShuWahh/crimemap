1a65514211aeac59677da0126af904ecbb9bc3f8
ShuWahh <shuwahh@gmail.com>
2017-07-14 09:09:23 -0400

    Eskeleton Crimemap

--- db_setup.py
+++ db_setup.py
@@ -4,7 +4,8 @@ import dbconfig
 connection = pymysql.connect(host = 'localhost', user = dbconfig.db_user, passwd = dbconfig.db_password)
 try:
     with connection.cursor() as cursor:
-        sql = "CREATE DATABASE IF NOT EXISTS crimemap" cursor.execute(sql)
+        sql = "CREATE DATABASE IF NOT EXISTS crimemap" 
+        cursor.execute(sql)
         sql = """CREATE TABLE IF NOT EXISTS crimemap.crimes(
             id int NOT NULL AUTO_INCREMENT,
             latitude FLOAT(10 , 6),
@@ -14,7 +15,8 @@ try:
             description VARCHAR(1000),
             updated_at TIMESTAMP,
             PRIMARY KEY (id)
-        )""" cursor.execute(sql)
+        )""" 
+        cursor.execute(sql)
         connection.commit()
 finally:
     connection.close()
